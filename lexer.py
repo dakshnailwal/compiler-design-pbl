738e03a0175c9f170b77aa58b7d1ea67266b925b
Daksh Nailwal <148696989+dakshnailwal@users.noreply.github.com>
2026-05-01 09:48:30 +0530

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -95,7 +95,7 @@ def tokenize(code):
         # ASSIGNMENT
         # ---------------------------------
         if char == "=":
-            tokens.append(Token(ASSIGN, char))
+            tokens.append(Token(ASSIGNMENT, char))
             i += 1
             continue
 
